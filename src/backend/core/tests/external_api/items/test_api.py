89fa805da5db635e2e1a39d41a5adc3240e30c46
Manuel Raynaud <manu@raynaud.io>
2025-05-23 15:20:15 +0200

    ✨(back) expose an external api using resource server authentication
    We expose now an external API. This external API can be used using a
    resource server authentication. The urls are prefixed with
    /external_api/v1.0 and for now the viewsets available are the Item and
    ItemAccess.

---
+++ src/backend/core/tests/external_api/items/test_api.py
@@ -0,0 +1,207 @@
+"""Tests for the Resource Server API for items."""
+
+import base64
+
+import pytest
+from rest_framework.test import APIClient
+
+from core import factories
+
+pytestmark = pytest.mark.django_db
+
+# pylint: disable=unused-argument
+
+
+@pytest.fixture(name="user_specific_sub")
+def fixture_user_specific_sub():
+    """
+    A fixture to create a user token for testing.
+    """
+    user = factories.UserFactory(sub="very-specific-sub")
+
+    yield user
+
+
+def build_authorization_bearer(token):
+    """
+    Build an Authorization Bearer header value from a token.
+
+    This can be used like this:
+    client.post(
+        ...
+        HTTP_AUTHORIZATION=f"Bearer {build_authorization_bearer('some_token')}",
+    )
+    """
+    return base64.b64encode(token.encode("utf-8")).decode("utf-8")
+
+
+@pytest.fixture(name="user_token")
+def fixture_user_token():
+    """
+    A fixture to create a user token for testing.
+    """
+    return build_authorization_bearer("some_token")
+
+
+def test_api_items_retrieve_anonymous_public_standalone():
+    """Anonymous users should not be allowed to retrieve an item."""
+    item = factories.ItemFactory(link_reach="public")
+
+    response = APIClient().get(f"/external_api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_retrieve_connected_not_resource_server():
+    """
+    Connected users should not be allowed to retrieve an item if they are not a resource server.
+    """
+    user = factories.UserFactory()
+    client = APIClient()
+    client.force_login(user)
+
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_retrieve_connected_resource_server(
+    user_token, resource_server_backend, user_specific_sub
+):
+    """Connected users should be allowed to retrieve an item if they are a resource server."""
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 200
+
+
+def test_api_items_retrieve_connected_resource_server_with_invalid_token(
+    user_token, resource_server_backend
+):
+    """User with an invalid sub should not be allowed to retrieve an item."""
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_retrieve_connected_resource_server_with_wrong_abilities(
+    user_token, user_specific_sub, resource_server_backend
+):
+    """User with wrong abilities should not be allowed to retrieve an item."""
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="restricted")
+    factories.UserItemAccessFactory(item=item, user=user_specific_sub, role="reader")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 200
+
+
+def test_api_items_retrieve_fetch_api_using_access_token(user_token, user_specific_sub):
+    """
+    User with an access token should not be allowed to retrieve an item from
+    the api endpoint.
+    """
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="restricted")
+    factories.UserItemAccessFactory(item=item, user=user_specific_sub, role="reader")
+
+    response = client.get(f"/api/v1.0/items/{item.id!s}/")
+
+    assert response.status_code == 403
+
+
+def test_api_items_accesses_retrieve_anonymous_public_standalone():
+    """Anonymous users should not be allowed to retrieve an item."""
+    item = factories.ItemFactory(link_reach="public")
+
+    response = APIClient().get(f"/external_api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_accesses_retrieve_connected_not_resource_server():
+    """
+    Connected users should not be allowed to retrieve an item if they are not a resource server.
+    """
+    user = factories.UserFactory()
+    client = APIClient()
+    client.force_login(user)
+
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_accesses_retrieve_connected_resource_server(
+    user_token, resource_server_backend, user_specific_sub
+):
+    """Connected users should be allowed to retrieve an item if they are a resource server."""
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 200
+
+
+def test_api_items_accesses_retrieve_connected_resource_server_with_invalid_token(
+    user_token, resource_server_backend
+):
+    """User with an invalid sub should not be allowed to retrieve an item."""
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="public")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 401
+
+
+def test_api_items_accesses_retrieve_connected_resource_server_with_wrong_abilities(
+    user_token, user_specific_sub, resource_server_backend
+):
+    """User with wrong abilities should not be allowed to retrieve an item."""
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="restricted")
+    factories.UserItemAccessFactory(item=item, user=user_specific_sub, role="reader")
+
+    response = client.get(f"/external_api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 200
+
+
+def test_api_items_accesses_retrieve_fetch_api_using_access_token(
+    user_token, user_specific_sub
+):
+    """
+    User with an access token should not be allowed to retrieve an item from
+    the api endpoint.
+    """
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {user_token}")
+    item = factories.ItemFactory(link_reach="restricted")
+    factories.UserItemAccessFactory(item=item, user=user_specific_sub, role="reader")
+
+    response = client.get(f"/api/v1.0/items/{item.id!s}/accesses/")
+
+    assert response.status_code == 403
--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.urls import include, path, re_path
 
 from lasuite.oidc_login.urls import urlpatterns as oidc_urls
+from lasuite.oidc_resource_server.urls import urlpatterns as resource_server_urls
 from rest_framework.routers import DefaultRouter
 
 from core.api import viewsets
+from core.external_api import viewsets as external_api_viewsets
 
 # - Main endpoints
 router = DefaultRouter()
@@ -33,6 +35,20 @@ sdk_relay_router.register(
     basename="sdk_relay_events",
 )
 
+# - Resource server routes
+external_api_router = DefaultRouter()
+external_api_router.register(
+    "items",
+    external_api_viewsets.ResourceServerItemViewSet,
+    basename="resource_server_items",
+)
+
+external_api_item_related_router = DefaultRouter()
+external_api_item_related_router.register(
+    "accesses",
+    external_api_viewsets.ResourceServerItemAccessViewSet,
+    basename="resource_server_item_accesses",
+)
 
 urlpatterns = [
     path(
@@ -41,6 +57,7 @@ urlpatterns = [
             [
                 *router.urls,
                 *oidc_urls,
+                *resource_server_urls,
                 re_path(
                     r"^items/(?P<resource_id>[0-9a-z-]*)/",
                     include(item_related_router.urls),
@@ -51,3 +68,19 @@ urlpatterns = [
     ),
     path(f"api/{settings.API_VERSION}/config/", viewsets.ConfigView.as_view()),
 ]
+
+if settings.OIDC_RESOURCE_SERVER_ENABLED:
+    urlpatterns.append(
+        path(
+            f"external_api/{settings.API_VERSION}/",
+            include(
+                [
+                    *external_api_router.urls,
+                    re_path(
+                        r"^items/(?P<resource_id>[0-9a-z-]*)/",
+                        include(external_api_item_related_router.urls),
+                    ),
+                ]
+            ),
+        )
+    )
