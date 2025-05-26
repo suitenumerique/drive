3ae3f0c90cbbcc9c06f2e2957ec0c1560e272693
Manuel Raynaud <manu@raynaud.io>
2025-05-26 16:31:40 +0200

    ✨(back) implement the WOPI host CheckFileInfo method
    The first method to implement is the CheckFileInfo method. Its
    documentation can be consulted with this link:
    https://learn.microsoft.com/en-us/microsoft-365/cloud-storage-partner-program/rest/files/checkfileinfo

---
+++ src/backend/wopi/authentication.py
@@ -0,0 +1,41 @@
+"""Authentication for WOPI requests."""
+
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+from wopi.services.access import AccessError, AccessUserItemService
+
+
+class WopiAccessTokenAuthentication(BaseAuthentication):
+    """
+    WOPI access token authentication.
+    """
+
+    def _get_access_token(self, request):
+        """Look for the access_token in both headers and query params."""
+        access_token = request.headers.get(
+            "Authorization", request.query_params.get("access_token")
+        )
+
+        if access_token and access_token.startswith("Bearer "):
+            access_token = access_token[7:]
+
+        return access_token
+
+    def authenticate(self, request):
+        """
+        Authenticate the request.
+        """
+        # First check if the access token is present in the request
+        access_token = self._get_access_token(request)
+        if not access_token:
+            raise AuthenticationFailed("Access token not provided")
+
+        # Check if the access token exists in cache
+        service = AccessUserItemService()
+        try:
+            access_user_item = service.get_access_user_item(access_token)
+        except AccessError as err:
+            raise AuthenticationFailed("Invalid access token") from err
+
+        return (access_user_item.user, access_user_item)
