37041e1f59ed8cc502eb221623ac9eede2d090cb
Nathan Panchout <nathanpanchout@hotmail.com>
2025-07-08 15:42:44 +0200

    ✨(front) add file preview and controls components
    Introduced a comprehensive file preview system that includes a new
    FilePreview component capable of displaying various file types such as
    images, videos, audio, and PDFs. The component features navigation
    controls for switching between files, a sidebar for additional content,
    and a download functionality. Additionally, a PreviewControls component
    was added to manage playback controls for video and audio files,
    complete with keyboard shortcuts for enhanced user experience.
    Corresponding SCSS styles have been created to ensure a cohesive and
    responsive design across all components.

---
+++ src/frontend/apps/drive/src/features/ui/preview/components/controls/PreviewControls.tsx
@@ -0,0 +1,110 @@
+import { Button } from "@openfun/cunningham-react";
+import { Icon } from "@gouvfr-lasuite/ui-kit";
+import { VolumeBar } from "../volume-bar/VolumeBar";
+import { useEffect } from "react";
+
+export type PreviewControlsProps = {
+  togglePlay: () => void;
+  isPlaying: boolean;
+  rewind10Seconds: () => void;
+  forward10Seconds: () => void;
+  volume: number;
+  isMuted: boolean;
+  toggleMute: () => void;
+  handleVolumeChange: (newVolume: number) => void;
+  toggleFullscreen: () => void;
+  isFullscreen: boolean;
+  showFullscreenBtn?: boolean;
+};
+
+export const PreviewControls = ({
+  togglePlay,
+  isPlaying,
+  rewind10Seconds,
+  forward10Seconds,
+  volume,
+  isMuted,
+  toggleMute,
+  handleVolumeChange,
+  toggleFullscreen,
+  isFullscreen,
+  showFullscreenBtn = false,
+}: PreviewControlsProps) => {
+  // Keyboard controls
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // Only handle keyboard events when video player is focused or when not in fullscreen
+      if (isFullscreen) return;
+
+      switch (event.code) {
+        case "Space":
+          event.preventDefault();
+          togglePlay();
+          break;
+        case "ArrowLeft":
+          event.preventDefault();
+          rewind10Seconds();
+          break;
+        case "ArrowRight":
+          event.preventDefault();
+          forward10Seconds();
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [togglePlay, rewind10Seconds, forward10Seconds]);
+  return (
+    <div className="suite-preview-controls">
+      <Button
+        color="tertiary-text"
+        onClick={togglePlay}
+        className="suite-preview-controls__btn"
+        icon={<Icon name={isPlaying ? "pause" : "play_arrow"} />}
+      />
+      <VerticalSeparator />
+      <Button
+        color="tertiary-text"
+        onClick={rewind10Seconds}
+        className="suite-preview-controls__btn"
+        icon={<Icon name={"replay_10"} />}
+      />
+      <Button
+        color="tertiary-text"
+        onClick={forward10Seconds}
+        className="suite-preview-controls__btn"
+        icon={<Icon name={"forward_10"} />}
+      />
+      <VerticalSeparator />
+
+      <VolumeBar
+        volume={volume}
+        isMuted={isMuted}
+        toggleMute={toggleMute}
+        handleVolumeChange={handleVolumeChange}
+      />
+
+      {showFullscreenBtn && (
+        <>
+          <VerticalSeparator />
+
+          <Button
+            color="tertiary-text"
+            onClick={toggleFullscreen}
+            className="suite-preview-controls__btn"
+            icon={
+              <Icon name={isFullscreen ? "fullscreen_exit" : "fullscreen"} />
+            }
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+const VerticalSeparator = () => {
+  return <div className="controls-vertical-separator" />;
+};
---
+++ src/frontend/apps/drive/src/features/ui/preview/files-preview/FilesPreview.tsx
@@ -0,0 +1,243 @@
+import {
+  getMimeCategory,
+  MimeCategory,
+  removeFileExtension,
+} from "@/features/explorer/utils/mimeTypes";
+import { Icon, IconType } from "@gouvfr-lasuite/ui-kit";
+import { Button } from "@openfun/cunningham-react";
+import React, { useEffect, useMemo, useState } from "react";
+import { createPortal } from "react-dom";
+import { ImageViewer } from "../image-viewer/ImageViewer";
+import { VideoPlayer } from "../video-player/VideoPlayer";
+import { AudioPlayer } from "../audio-player/AudioPlayer";
+import { PreviewPdf } from "../pdf-preview/PreviewPdf";
+
+import { NotSupportedPreview } from "../not-supported/NotSupportedPreview";
+import { getIconByMimeType } from "@/features/explorer/components/icons/ItemIcon";
+import { useTranslation } from "react-i18next";
+
+export type FilePreviewType = {
+  id: string;
+  title: string;
+  mimetype: string;
+  url: string;
+};
+
+type FilePreviewData = FilePreviewType & {
+  category: MimeCategory;
+};
+
+interface FilePreviewProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  files?: FilePreviewType[];
+  initialIndexFile?: number;
+  openedFileId?: string;
+  headerRightContent?: React.ReactNode;
+  sidebarContent?: React.ReactNode;
+  onChangeFile?: (file?: FilePreviewType) => void;
+}
+
+export const FilePreview = ({
+  isOpen,
+  onClose,
+  title = "File Preview",
+  files = [],
+  initialIndexFile = 0,
+  openedFileId,
+  sidebarContent,
+  headerRightContent,
+  onChangeFile,
+}: FilePreviewProps) => {
+  const { t } = useTranslation();
+  const [currentIndex, setCurrentIndex] = useState(initialIndexFile);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const data: FilePreviewData[] = useMemo(() => {
+    return files?.map((file) => ({
+      ...file,
+      category: getMimeCategory(file.mimetype),
+    }));
+  }, [files]);
+
+  const currentFile: FilePreviewData | null = data[currentIndex] || null;
+
+  const handleDownload = async () => {
+    // Temporary solution, waiting for a proper download_url attribute.
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = currentFile.url;
+    a.target = "_blank";
+
+    a.download = `${currentFile.title}.${currentFile.mimetype}`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  // Render the appropriate viewer based on file category
+  const renderViewer = () => {
+    if (!currentFile) {
+      return <div>{t("file_preview.unsupported.title")}</div>;
+    }
+
+    switch (currentFile.category) {
+      case MimeCategory.IMAGE:
+        return (
+          <ImageViewer
+            src={currentFile.url}
+            alt={currentFile.title}
+            className="file-preview-viewer"
+          />
+        );
+      case MimeCategory.VIDEO:
+        return (
+          <div className="video-preview-viewer-container">
+            <div className="video-preview-viewer">
+              <VideoPlayer
+                src={currentFile.url}
+                className="file-preview-viewer"
+                controls={true}
+              />
+            </div>
+          </div>
+        );
+      case MimeCategory.AUDIO:
+        return (
+          <div className="video-preview-viewer-container">
+            <div className="video-preview-viewer">
+              <AudioPlayer
+                src={currentFile.url}
+                title={currentFile.title}
+                className="file-preview-viewer"
+              />
+            </div>
+          </div>
+        );
+      case MimeCategory.PDF:
+        return <PreviewPdf src={currentFile.url} title={currentFile.title} />;
+      default:
+        return <NotSupportedPreview file={currentFile} />;
+    }
+  };
+
+  useEffect(() => {
+    if (openedFileId) {
+      const index = data.findIndex((file) => file.id === openedFileId);
+      setCurrentIndex(index > -1 ? index : 0);
+    }
+  }, [openedFileId]);
+
+  useEffect(() => {
+    onChangeFile?.(currentFile);
+  }, [currentFile]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const previewContent = (
+    <div className="file-preview-overlay">
+      <div
+        className={`file-preview-container ${
+          isSidebarOpen ? "sidebar-open" : ""
+        }`}
+      >
+        <div className="file-preview-header">
+          <div className="file-preview-header__content">
+            <div className="file-preview-header__content-left">
+              <Button
+                color="tertiary-text"
+                size="small"
+                onClick={onClose}
+                icon={<Icon name="close" />}
+              />
+
+              <div className="file-preview-title">
+                <img
+                  src={getIconByMimeType(currentFile.mimetype, "mini").src}
+                  alt=""
+                  width={24}
+                  className={`item-icon`}
+                  draggable="false"
+                />
+                <h1 className="file-preview-title">
+                  {removeFileExtension(currentFile?.title || title)}
+                </h1>
+              </div>
+            </div>
+            <div className="file-preview-header__content-center">
+              <FilePreviewNav
+                currentIndex={currentIndex}
+                totalFiles={data.length}
+                onPrevious={() => setCurrentIndex(currentIndex - 1)}
+                onNext={() => setCurrentIndex(currentIndex + 1)}
+              />
+            </div>
+            <div className="file-preview-header__content-right">
+              {headerRightContent}
+              <Button
+                color="tertiary-text"
+                onClick={handleDownload}
+                icon={<Icon type={IconType.OUTLINED} name={"file_download"} />}
+              />
+
+              <Button
+                color="tertiary-text"
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                icon={<Icon name={"info_outline"} />}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="file-preview-content">
+          <div className="file-preview-main">{renderViewer()}</div>
+
+          <div
+            className={`file-preview-sidebar ${isSidebarOpen ? "open" : ""}`}
+          >
+            {sidebarContent}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  // Use Portal to render the preview outside the component hierarchy
+  return createPortal(previewContent, document.body);
+};
+
+interface FilePreviewNavProps {
+  currentIndex: number;
+  totalFiles: number;
+  onPrevious: () => void;
+  onNext: () => void;
+}
+
+export const FilePreviewNav: React.FC<FilePreviewNavProps> = ({
+  currentIndex,
+  totalFiles,
+  onPrevious,
+  onNext,
+}) => {
+  return (
+    <div className="file-preview-nav">
+      <Button
+        color="tertiary-text"
+        onClick={onPrevious}
+        disabled={currentIndex === 0}
+        icon={<Icon name="arrow_back" />}
+      />
+      <span className="file-preview-nav__count">
+        {currentIndex + 1} / {totalFiles}
+      </span>
+      <Button
+        color="tertiary-text"
+        onClick={onNext}
+        disabled={currentIndex === totalFiles - 1}
+        icon={<Icon name="arrow_forward" />}
+      />
+    </div>
+  );
+};
