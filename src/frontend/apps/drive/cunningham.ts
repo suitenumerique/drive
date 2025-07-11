306d3075006cca387becbb3f7c2cd32ca2aa6759
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-11 15:02:04 +0200

    ✨(front) make logos themable
    The logos were hard coded and this update in the config will
    allow to make them custom with Cunningham.

--- src/frontend/apps/drive/cunningham.ts
+++ src/frontend/apps/drive/cunningham.ts
@@ -1,5 +1,38 @@
 import { cunninghamConfig } from "@gouvfr-lasuite/ui-kit";
 
-export default {
+const config = {
   ...cunninghamConfig,
+  themes: {
+    ...cunninghamConfig.themes,
+    default: {
+      ...cunninghamConfig.themes.default,
+      components: {
+        ...cunninghamConfig.themes.default.components,
+        favicon: {
+          src: "'/assets/favicon.png'",
+        },
+        logo: {
+          src: "url('/assets/logo_alpha.svg')",
+        },
+        "logo-icon": {
+          src: "url('/assets/logo-icon_alpha.svg')",
+        },
+      },
+    },
+    anct: {
+      components: {
+        favicon: {
+          src: "'/assets/anct_favicon.png'",
+        },
+        logo: {
+          src: "url('/assets/anct_logo_alpha.svg')",
+        },
+        "logo-icon": {
+          src: "url('/assets/anct_logo-icon.svg')",
+        },
+      },
+    },
+  },
 };
+
+export default config;
