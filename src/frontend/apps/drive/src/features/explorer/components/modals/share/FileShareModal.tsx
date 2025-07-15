d4963f973c4c749db3f6fb521fdab52d76945fa2
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-15 16:28:14 +0200

    ✨(front) add share modal for file Item
    Because when picking file from the file picker puts its link_role to
    public, we need a way to rollback it if needed.

---
+++ src/frontend/apps/drive/src/features/explorer/components/modals/share/FileShareModal.tsx
@@ -0,0 +1,50 @@
+import { Item, LinkReach } from "@/features/drivers/types";
+import { useMutationUpdateItem } from "@/features/explorer/hooks/useMutations";
+import { useClipboard } from "@/hooks/useCopyToClipboard";
+import { ShareModal, ShareModalCopyLinkFooter } from "@gouvfr-lasuite/ui-kit";
+import { ModalProps } from "@openfun/cunningham-react";
+
+type FileShareModalProps = Pick<ModalProps, "isOpen" | "onClose"> & {
+  item: Item;
+};
+
+export const FileShareModal = ({ item, ...props }: FileShareModalProps) => {
+  const updateItem = useMutationUpdateItem();
+  const copyToClipboard = useClipboard();
+  return (
+    <ShareModal
+      linkSettings={true}
+      linkReachChoices={[
+        {
+          value: "public",
+        },
+        {
+          value: "restricted",
+        },
+      ]}
+      linkReach={item.link_reach}
+      onUpdateLinkReach={(value) => {
+        updateItem.mutate({
+          id: item.id,
+          link_reach: value as LinkReach,
+        });
+      }}
+      canUpdate={item.abilities.update}
+      hideInvitations={true}
+      hideMembers={true}
+      outsideSearchContent={
+        <ShareModalCopyLinkFooter
+          onCopyLink={() => {
+            copyToClipboard(item.url ?? "");
+          }}
+          onOk={() => {
+            props.onClose();
+          }}
+        />
+      }
+      {...props}
+    >
+      COUCOU
+    </ShareModal>
+  );
+};
