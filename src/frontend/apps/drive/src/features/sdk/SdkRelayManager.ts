cf4a1d20e0231e85053759eed2e90fc320a02692
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-15 16:28:47 +0200

    ✨(front) add SDKRelayManager
    This manager is used to pass data from the file picker popup to its
    parent window, handled by the sdk package.

---
+++ src/frontend/apps/drive/src/features/sdk/SdkRelayManager.ts
@@ -0,0 +1,29 @@
+import { fetchAPI } from "../api/fetchApi";
+
+export enum ClientMessageType {
+  // Picker.
+  ITEMS_SELECTED = "ITEMS_SELECTED",
+  CANCEL = "CANCEL",
+  // Saver
+  SAVER_READY = "SAVER_READY",
+  SAVER_PAYLOAD = "SAVER_PAYLOAD",
+  ITEM_SAVED = "ITEM_SAVED",
+}
+
+export interface SDKRelayEvent {
+  type: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
+}
+
+export class SDKRelayManager {
+  static async registerEvent(token: string, event: SDKRelayEvent) {
+    return await fetchAPI(`sdk-relay/events/`, {
+      method: "POST",
+      body: JSON.stringify({
+        token,
+        event,
+      }),
+    });
+  }
+}
