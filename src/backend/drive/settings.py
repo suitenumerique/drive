279fa69a92983240a5551d4f317a2bf286ff6897
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-16 14:34:59 +0200

    🔧(back) update settings cache
    We need by default a reliable redis cache in order to make the
    sdk relay work. With the previous cache it does not work.

--- src/backend/drive/settings.py
+++ src/backend/drive/settings.py
@@ -726,12 +726,9 @@ class Development(Base):
     SESSION_COOKIE_NAME = "drive_sessionid"
 
     USE_SWAGGER = True
-    SESSION_CACHE_ALIAS = "session"
+
     CACHES = {
         "default": {
-            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
-        },
-        "session": {
             "BACKEND": "django_redis.cache.RedisCache",
             "LOCATION": values.Value(
                 "redis://redis:6379/2",
