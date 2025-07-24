6d913ed1c315c288d2d6b554890ef50c73c4b7a0
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-24 15:45:16 +0200

    ✨(front) add SimpleLayout
    It is used to display the header and provide Auth context to the children.
    Nothing fancy, just a simple layout.

---
+++ src/frontend/apps/drive/src/features/layouts/components/simple/SimpleLayout.tsx
@@ -0,0 +1,33 @@
+import { MainLayout } from "@gouvfr-lasuite/ui-kit";
+import { GlobalLayout } from "../global/GlobalLayout";
+import { HeaderIcon, HeaderRight } from "../header/Header";
+import { Toaster } from "@/features/ui/components/toaster/Toaster";
+import { LeftPanelMobile } from "@/features/layouts/components/left-panel/LeftPanelMobile";
+
+export const getSimpleLayout = (page: React.ReactElement) => {
+  return <SimpleLayout>{page}</SimpleLayout>;
+};
+
+/**
+ * This layout is used for the simple pages.
+ * It is used to display the header and provide
+ * Auth context to the children.
+ */
+export const SimpleLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <GlobalLayout>
+        <MainLayout
+          enableResize
+          hideLeftPanelOnDesktop={true}
+          leftPanelContent={<LeftPanelMobile />}
+          icon={<HeaderIcon />}
+          rightHeaderContent={<HeaderRight />}
+        >
+          {children}
+          <Toaster />
+        </MainLayout>
+      </GlobalLayout>
+    </div>
+  );
+};
