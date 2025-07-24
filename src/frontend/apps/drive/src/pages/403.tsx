e74efe06c8a141d1a11045fee023dc7596c08376
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-24 15:45:37 +0200

    ✨(front) add 403 page
    This page provides information to the user related to 403 error.

---
+++ src/frontend/apps/drive/src/pages/403.tsx
@@ -0,0 +1,21 @@
+import { getSimpleLayout } from "@/features/layouts/components/simple/SimpleLayout";
+import { GenericDisclaimer } from "@/features/ui/components/generic-disclaimer/GenericDisclaimer";
+import { Icon } from "@gouvfr-lasuite/ui-kit";
+import { Button } from "@openfun/cunningham-react";
+import { useTranslation } from "react-i18next";
+
+export default function UnauthorizedPage() {
+  const { t } = useTranslation();
+  return (
+    <GenericDisclaimer
+      message={t("403.title")}
+      imageSrc="/assets/403-background.png"
+    >
+      <Button href="/" icon={<Icon name="home" />}>
+        {t("403.button")}
+      </Button>
+    </GenericDisclaimer>
+  );
+}
+
+UnauthorizedPage.getLayout = getSimpleLayout;
