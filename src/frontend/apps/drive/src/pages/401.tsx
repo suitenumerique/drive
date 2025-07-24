0b925eab269eacb7c6efc51afc2538da58bc1f3e
Nathan Vasse <nathan.vasse@gmail.com>
2025-07-24 15:45:27 +0200

    ✨(front) add 401 page
    This page provides information to the user related to 401 error.

---
+++ src/frontend/apps/drive/src/pages/401.tsx
@@ -0,0 +1,19 @@
+import { login } from "@/features/auth/Auth";
+import { getSimpleLayout } from "@/features/layouts/components/simple/SimpleLayout";
+import { GenericDisclaimer } from "@/features/ui/components/generic-disclaimer/GenericDisclaimer";
+import { Button } from "@openfun/cunningham-react";
+import { useTranslation } from "react-i18next";
+
+export default function UnauthorizedPage() {
+  const { t } = useTranslation();
+  return (
+    <GenericDisclaimer
+      message={t("401.title")}
+      imageSrc="/assets/401-background.png"
+    >
+      <Button onClick={() => login()}>{t("401.button")}</Button>
+    </GenericDisclaimer>
+  );
+}
+
+UnauthorizedPage.getLayout = getSimpleLayout;
