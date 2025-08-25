f2ad27c03d42bda58fcc0acc1f2f290a72783d66
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-25 14:10:31 +0200

    ✨(frontend) add useDownloadItem hook for file download handling
    Introduced a new custom hook, useDownloadItem, to manage file downloads.
    This hook validates item properties, handles different upload states,
    and displays confirmation modals for suspicious or large files before
    proceeding with the download. It integrates translation support for user
    notifications.

---
+++ src/frontend/apps/drive/src/features/items/hooks/useDownloadItem.tsx
@@ -0,0 +1,66 @@
+import { Item, ItemUploadState } from "@/features/drivers/types";
+import { ModalSize, useModals } from "@openfun/cunningham-react";
+import { downloadFile } from "../utils";
+import { useAuth } from "@/features/auth/Auth";
+import { useTranslation } from "react-i18next";
+import {
+  addToast,
+  ToasterItem,
+} from "@/features/ui/components/toaster/Toaster";
+
+export const useDownloadItem = () => {
+  const { t } = useTranslation();
+
+  const modals = useModals();
+  const { user } = useAuth();
+  const handleDownloadItem = async (item?: Item) => {
+    if (!item?.url || !item?.title) {
+      addToast(
+        <ToasterItem type="error">
+          <span className="material-icons">error</span>
+          <span>{t("file_download_modal.error.no_url_or_title")}</span>
+        </ToasterItem>
+      );
+      return;
+    }
+
+    const itemState = item?.upload_state;
+    const isCreator = item?.creator.id === user?.id;
+    const isFileTooLarge =
+      itemState === ItemUploadState.FILE_TOO_LARGE_TO_ANALYZE;
+    const isSuspicious = itemState === ItemUploadState.SUSPICIOUS;
+    const isPending =
+      itemState === ItemUploadState.PENDING ||
+      itemState === ItemUploadState.ANALYZING;
+
+    let title: string | undefined;
+    let description: string | undefined;
+
+    if (isCreator && isSuspicious) {
+      title = t("file_download_modal.suspicious.title");
+      description = t("file_download_modal.description");
+    } else if (!isCreator && (isFileTooLarge || isPending)) {
+      title = isFileTooLarge
+        ? t("file_download_modal.file_too_large.title")
+        : t("file_download_modal.analyzing.title");
+      description = t("file_download_modal.description");
+    }
+
+    if (title && description) {
+      const decision = await modals.confirmationModal({
+        size: ModalSize.MEDIUM,
+        title,
+        children: description,
+      });
+
+      if (decision === "yes") {
+        // Only call downloadFile if both url and title are defined
+        downloadFile(item.url, item.title);
+      }
+    } else {
+      downloadFile(item.url, item.title);
+    }
+  };
+
+  return { handleDownloadItem };
+};
