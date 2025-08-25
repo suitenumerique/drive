1b171f19b12a2f9f24a242944fb530fe8227a301
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-25 14:10:09 +0200

    ✨(frontend) add SuspiciousPreview component and styles
    Introduced a new SuspiciousPreview component along with its associated
    styles. This component displays a warning for suspicious files,
    including a title, description, and a download button. The component
    utilizes translations for user notifications and integrates a new SCSS
    file for styling.

---
+++ src/frontend/apps/drive/src/features/ui/preview/suspicious/SuspiciousPreview.tsx
@@ -0,0 +1,42 @@
+import { useTranslation } from "react-i18next";
+
+import { Button } from "@openfun/cunningham-react";
+import { Icon, IconType } from "@gouvfr-lasuite/ui-kit";
+import mimeSuspicious from "@/assets/files/icons/suspicious_file.svg";
+
+interface SuspiciousPreviewProps {
+  handleDownload?: () => void;
+}
+
+export const SuspiciousPreview = ({
+  handleDownload,
+}: SuspiciousPreviewProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="file-preview-suspicious">
+      <div className="file-preview-suspicious__icon">
+        <img src={mimeSuspicious.src} alt="" className="item-icon xlarge" />
+      </div>
+      <span className="file-preview-suspicious__title">
+        {t("file_preview.suspicious.title")}
+      </span>
+      <span className="file-preview-suspicious__description">
+        {t("file_preview.suspicious.description")}
+      </span>
+
+      {handleDownload && (
+        <Button
+          color="tertiary"
+          className="file-preview-suspicious__download-button"
+          icon={
+            <Icon name="file_download" type={IconType.OUTLINED} size={16} />
+          }
+          onClick={handleDownload}
+        >
+          {t("file_preview.unsupported.download")}
+        </Button>
+      )}
+    </div>
+  );
+};
