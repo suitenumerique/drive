882c19289115f4d4a862681f320a0407c145552a
Sylvain Gizard <sylvain.gizard@gmail.com>
2025-10-16 11:09:32 +0200

    override page_size max

--- src/frontend/apps/drive/src/features/drivers/implementations/StandardDriver.ts
+++ src/frontend/apps/drive/src/features/drivers/implementations/StandardDriver.ts
@@ -28,7 +28,7 @@ export class StandardDriver extends Driver {
 
   async getItems(filters = {}): Promise<Item[]> {
     const response = await fetchAPI(`items/`, {
-      params: filters,
+      params: { ...filters, page_size: "100000" },
     });
     const data = await response.json();
     return jsonToItems(data.results);
