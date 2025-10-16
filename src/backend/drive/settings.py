08c51b7918b31874fd0767df20ff4cabca480d67
Fabre Florian <ffabre@hybird.org>
2025-10-16 11:44:58 +0200

    ✨(backend) add demo populate command
    Add a new Django app 'demo' that contains the command 'create_demo'
    Generate dummy users and files with existing factories.
    
    Signed-off-by: Fabre Florian <ffabre@hybird.org>

--- src/backend/drive/settings.py
+++ src/backend/drive/settings.py
@@ -936,6 +936,7 @@ class Development(Base):
             "drf_spectacular_sidecar",
             "debug_toolbar",
             "e2e",
+            "demo",
         ]
 
 
@@ -956,7 +957,7 @@ class Test(Base):
 
     def __init__(self):
         # pylint: disable=invalid-name
-        self.INSTALLED_APPS += ["drf_spectacular_sidecar", "e2e"]
+        self.INSTALLED_APPS += ["drf_spectacular_sidecar", "e2e", "demo"]
 
 
 class ContinuousIntegration(Test):
