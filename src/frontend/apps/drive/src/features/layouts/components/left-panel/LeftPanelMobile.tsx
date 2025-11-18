aba4418671987ffaedee394d6794896ab9ca7e81
Mélanie Ngo <melanie.ngo@interstis.fr>
2025-11-18 16:49:26 +0100

    ✨(front) apply config to language picker display
    Use configuration to hide language picker if necessary.
    
    Signed-off-by: Mélanie Ngo <melanie.ngo@interstis.fr>

--- src/frontend/apps/drive/src/features/layouts/components/left-panel/LeftPanelMobile.tsx
+++ src/frontend/apps/drive/src/features/layouts/components/left-panel/LeftPanelMobile.tsx
@@ -2,12 +2,15 @@ import { LanguagePicker } from "../header/Header";
 import { LogoutButton } from "@/features/auth/components/LogoutButton";
 import { useAuth } from "@/features/auth/Auth";
 import { LoginButton } from "@/features/auth/components/LoginButton";
+import { useConfig } from "@/features/config/ConfigProvider";
 
 export const LeftPanelMobile = () => {
   const { user } = useAuth();
+  const { config } = useConfig();
+
   return (
     <div className="drive__home__left-panel">
-      <LanguagePicker />
+      {!config?.FRONTEND_HIDE_LANGUAGE_PICKER && <LanguagePicker />}
       {user ? <LogoutButton /> : <LoginButton />}
     </div>
   );
