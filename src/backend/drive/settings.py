224551d3b6a30cf961b4ed096e3c5c45b9705d33
Mélanie Ngo <melanie.ngo@interstis.fr>
2025-11-18 16:46:21 +0100

    ✨(back) add FRONTEND_HIDE_LANGUAGE_PICKER envvar
    Variable to define language picker visibility.
    
    Signed-off-by: Mélanie Ngo <melanie.ngo@interstis.fr>

--- src/backend/drive/settings.py
+++ src/backend/drive/settings.py
@@ -459,6 +459,9 @@ class Base(Configuration):
     FRONTEND_HIDE_GAUFRE = values.BooleanValue(
         default=False, environ_name="FRONTEND_HIDE_GAUFRE", environ_prefix=None
     )
+    FRONTEND_HIDE_LANGUAGE_PICKER = values.BooleanValue(
+        default=False, environ_name="FRONTEND_HIDE_LANGUAGE_PICKER", environ_prefix=None
+    )
     THEME_CUSTOMIZATION_FILE_PATH = values.Value(
         os.path.join(BASE_DIR, "drive/configuration/theme/default.json"),
         environ_name="THEME_CUSTOMIZATION_FILE_PATH",
