4ca9bc7d23c8d55e1e98afe91d6bbd77c0fb1250
Nathan Vasse <nathan.vasse@gmail.com>
2025-11-24 17:40:37 +0100

    fixup! ✨(back) add entitlement backends

--- src/backend/core/entitlements/__init__.py
+++ src/backend/core/entitlements/__init__.py
@@ -2,10 +2,13 @@
 Entitlements backend utilities.
 """
 
+import functools
+
 from django.conf import settings
 from django.utils.module_loading import import_string
 
 
+@functools.cache
 def get_entitlements_backend():
     """
     Get the entitlements backend.
